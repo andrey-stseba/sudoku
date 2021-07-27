d0538622e012fee664e736a2fe8cbf7a3f050772
andrey-stseba <scebaandru@gmail.com>
2021-07-27 06:38:45 +0300

    1  project version

--- app.js
+++ app.js
@@ -22,3 +22,5 @@ const game = {
 
 game.createCells();
 game.start("medium");
+
+//https://webinar.web-academy.com.ua/stvoryuyemo-holovolomku-zahodynu-za-dopomohoyu-javascript-live?utm_source=smartsender&utm_medium=events&utm_campaign=free-webinar-for-fe-with-Borys-Cherepaha-21-07-2021&utm_content=free-webinar&utm_term=free-webinar-for-fe-with-Borys-Cherepaha-21-07-2021
